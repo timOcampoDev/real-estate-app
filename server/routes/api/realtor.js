eca76e6d30d91d0c7cc4e07a828bfc6be19debc3
TimOcampo <timocmpo@TimOcampos-MacBook-Pro.local>
2021-04-18 13:07:04 -0700

    wired up first realtor API call.

--- server/routes/api/realtor.js
+++ server/routes/api/realtor.js
@@ -1,5 +1,6 @@
 const express = require( 'express' );
 const router = express.Router();
+const unirest = require("unirest");
 
 /**
  * @route   GET api/realtor
@@ -7,5 +8,36 @@ const router = express.Router();
  */
 
 router.get( '/test' , (req, res)=> res.send( 'realtor WORKS' ));
+
+
+/**
+ * @route   GET api/query-listings
+ * @desc    Gets query result for property listings
+ */
+
+router.get( '/query-listings', (req, res)=>{
+    const request = unirest("GET", "https://realtor.p.rapidapi.com/locations/auto-complete"),
+        HOST = process.env.HOST_NAME_REALTOR,
+        SECRET= process.env.SECRET_REALTOR;
+
+    request.query({
+        // "input": [CITY, STATE]
+        "input": ["tempe", 'az']
+    });
+
+    request.headers({
+        "x-rapidapi-key": SECRET,
+        "x-rapidapi-host": HOST,
+        "useQueryString": true
+    });
+
+    request.end(function (res) {
+        if (res.error) throw new Error(res.error);
+
+        console.log(res.body);
+    });
+
+    return res.send( '/query-listings WORKS' )
+});
 
 module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 "use strict"
-const express = require('express')
-const app = express();
-const PORT = process.env.PORT || 5000;
+require('dotenv').config();
+const express = require('express'),
+app = express(),
+PORT = process.env.PORT || 5000;
 
 app.get('/' , (req, res)=> res.send('API WORKS'));
 
