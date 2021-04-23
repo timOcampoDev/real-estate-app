a6ae72d88dc1758f6b778c9dfa1b3cae4434914d
TimOcampo <timocmpo@TimOcampos-MacBook-Pro.local>
2021-04-23 11:30:12 -0700

    Hooked up google auto complete for SearchBar component.

--- src/containers/SearchBarComponent/SearchBarContainer.jsx
+++ src/containers/SearchBarComponent/SearchBarContainer.jsx
@@ -13,23 +13,44 @@ const SearchbarContainer =({props})=>{
     const [loaded, setLoaded] = useState(false)
     const [address, setAddress] = useState("")
 
-    const handleSelect = async (value)=>{}
+    const [coordinates, setCoordinates] = useState({
+        lat: null,
+        lng: null
+    })
+
+    const handleSelectAuoSugest = async (value)=>{
+        const results = await geocodeByAddress(value);
+        const latLng = await getLatLng( results[0] );
+        setAddress(value); // this will auto complete based to partial typing and suggestion
+        setCoordinates( latLng )
+        console.log( address )
+    }
 
     return(
         <div>
-            <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelect}>
-                {/*Note : see https://www.npmjs.com/package/react-places-autocomplete for the render props passed below */}
+            <PlacesAutocomplete value={address} onChange={setAddress} onSelect={handleSelectAuoSugest}>
+                {/*Note : see https://www.npmjs.com/package/react-places-autocomplete for details about the render props passed below */}
                 {({ getInputProps, suggestions, getSuggestionItemProps, loading})=>(
                     <div>
+
+                        <div>
+                            <p>Latitude : {coordinates.lat}</p>
+                            <p>Longitude : {coordinates.lng}</p>
+                        </div>
+
                         <input {...getInputProps({placeholder: "Type Address"})}/>
 
                         <div id={'auto-complete-suggestions-wrapper'}>
                             {loading ? <div>...loading</div> : null }
 
-                            {suggestions.map((suggestion)=>{
-                                return <div>{suggestion.description}</div>
+                            {suggestions.map((suggestion, idx)=>{
+                                const style={
+                                    background: suggestion.active ? "cyan" : 'black'}
+                                return(
+                                    <div key={idx} {...getSuggestionItemProps( suggestion, { style } )} >
+                                        {suggestion.description}
+                                    </div>)
                             })}
-
                         </div>
                     </div>
 
